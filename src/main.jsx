960ee8f1422dc0544d23b5d2dcce81a4d633ca12
saksham10884 <sakshamsharma25106@gmail.com>
2025-06-25 15:41:46 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="{/${process.env.PUBLIC_URL}}">
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
       <App />
     </BrowserRouter>
   </StrictMode>,
